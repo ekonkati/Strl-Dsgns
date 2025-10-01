5c1f5266bb540e2a74fc8718d2e4aae70bf9871c
DrEK <162624985+ekonkati@users.noreply.github.com>
2025-10-02 00:03:14 +0530

    Create home.py

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Structural Suite", layout="wide")
+st.title("Structural Design Suite")
+
+st.markdown("""
+Welcome! Use the **Pages** menu (left sidebar) to open:
+- **Water Tank Design (IS 3370 / IS 1893-2)**
+- **RCC Beam & Column Designer**
+""")
